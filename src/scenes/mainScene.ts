19b6555e7b4973b2a0eabcabe65b9923267cf31c
Christian R <rullan@udel.edu>
2024-02-24 17:38:31 -0500

    added platofrms

--- src/scenes/mainScene.ts
+++ src/scenes/mainScene.ts
@@ -1,6 +1,8 @@
 import Phaser from "phaser";
 
 export default class MainScene extends Phaser.Scene {
+    private platforms: Phaser.Physics.Arcade.StaticGroup;
+
     constructor() {
         super({ key: "MainScene" });
     }
@@ -13,7 +15,15 @@ export default class MainScene extends Phaser.Scene {
                 fontSize: "24px",
             })
             .setOrigin(1, 0);
+
+        //implementing assets into game
         this.add.image(400, 300, "sky");
+        this.platforms = this.physics.add.staticGroup();
+        this.platforms.create(400, 568, "platform").setScale(2).refreshBody();
+        this.platforms.create(600, 400, "platform");
+        this.platforms.create(50, 250, "platform");
+        this.platforms.create(750, 220, "platform");
+        //this.add.image(400, 300, "star");
     }
 
     update() {}
--- src/scenes/preloadScene.ts
+++ src/scenes/preloadScene.ts
@@ -7,7 +7,7 @@ export default class PreloadScene extends Phaser.Scene {
 
     preload() {
         this.load.image("sky", "assets/sky.png");
-        this.load.image("ground", "assets/ground.png");
+        this.load.image("platform", "assets/platform.png");
         this.load.image("star", "assets/star.png");
         this.load.image("bomb", "assets/bomb.png");
         this.load.spritesheet("dude", "assets/dude.png", {
